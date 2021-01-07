7410d832b301e6081529d30960bcf91ccce4d958
Manuel Duran <durbonca@gmail.com>
2021-01-07 17:57:08 -0300

    adding description and extra data

--- script.js
+++ script.js
@@ -22,6 +22,14 @@ const getBeers = async () => {
                             <span>IBU: ${beer.ibu}</span>
                         </span>
                     </div>
+                    <div class='beer__content'>
+                        <div class='beer__name'>${beer.name}</div>
+                        <div class='beer__tagline'>${beer.tagline}</div>
+                        <div class='beer__description'>${beer.description}</div>
+                        <div class='beer__food-pairing'>
+                            Pair with: ${beer.food_pairing.join(', ')}
+                        </div>
+                    </div>
                 </div>
                 `
                 beersList[0].appendChild(beerEntry)
